da42f602c7b0f73463e1f8904d36b4b1d207e11a
ANU7MADHAV <anumadhavan888@gmail.com>
2023-12-15 19:47:24 +0530

    sidebar added

--- components/Navbar/MobileNav.tsx
+++ components/Navbar/MobileNav.tsx
@@ -1,52 +1,77 @@
 "use client";
 
+import icon from "@/app/favicon.ico";
 import {
   Sheet,
   SheetClose,
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import { SignedOut } from "@clerk/nextjs";
+import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import Link from "next/link";
+// import { useParams } from "next/navigation";
 import { FaBars } from "react-icons/fa6";
-import icon from "@/app/favicon.ico";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { Button } from "@nextui-org/react";
 
 const NavContent = () => {
-  return <h1>NavContent</h1>;
+  // const params = useParams();
+  // console.log(params);
+  // const active = sidebarLinks.find((link) => link.label === params);
+
+  return (
+    <section className="my-4 flex flex-col gap-y-2 pl-2 text-left font-spaceGrotesk">
+      {sidebarLinks.map((link) => (
+        <Link key={link.label} href={link.route}>
+          <p className="hover:primary-gradient rounded-lg px-2 transition ease-in-out hover:scale-105">
+            {link.label}
+          </p>
+        </Link>
+      ))}
+    </section>
+  );
 };
 
 const MobileNav = () => {
   return (
     <Sheet>
       <SheetTrigger asChild className="sm:hidden">
-        <FaBars />
+        <div>
+          <FaBars />
+        </div>
       </SheetTrigger>
-      <SheetContent side="left">
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
         <Link href="/" className="flex">
           <Image src={icon} alt="icon bug" width={30} height={30} />
           <p className="h3-bold px-1 font-spaceGrotesk text-dark-100 dark:text-light-900 sm:block">
             Bug <span className="text-primary-500">Helper</span>
           </p>
         </Link>
-        <SheetClose>
+        <SheetClose asChild>
           <NavContent />
         </SheetClose>
-        <SheetClose>
-          <SignedOut>
-            <Link href="/sign-in">
-              <Button>Log In</Button>
-            </Link>
-          </SignedOut>
-        </SheetClose>
-        <SheetClose>
-          <SignedIn>
+        <div className="flex flex-col gap-3">
+          <SheetClose asChild>
+            <SignedOut>
+              <Link href="/sign-in">
+                <Button className="min-h-[41px] w-full rounded-lg px-4 py-3 font-spaceGrotesk">
+                  Log In
+                </Button>
+              </Link>
+            </SignedOut>
+          </SheetClose>
+          <SheetClose asChild>
             <Link href="/sign-out">
-              <Button>Log out</Button>
+              <Button className="min-h-[41px] w-full rounded-lg px-4 py-3 font-spaceGrotesk">
+                <span className="primary-text-gradient">Sign Up</span>
+              </Button>
             </Link>
-          </SignedIn>
-        </SheetClose>
+          </SheetClose>
+        </div>
       </SheetContent>
     </Sheet>
   );
